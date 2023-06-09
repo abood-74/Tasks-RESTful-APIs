c5f9279d742b0ddb77fcad204a7542fc3edb4fe5
abood-74 <abdelrahmanashour45@gmail.com>
2023-06-09 04:04:08 +0300

    adding pagination feature

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 from .models import Task
-
+from django.utils import timezone
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = '__all__'
+    
+    
--- tasks/views.py
+++ tasks/views.py
@@ -1,10 +1,17 @@
-from rest_framework import generics
+from rest_framework import generics, pagination
 from .models import Task
 from .serializers import TaskSerializer
+
+class TaskListPagination(pagination.PageNumberPagination):
+    page_size = 5
+    page_size_query_param = 'page_size'
+    max_page_size = 100
 
 class TaskList(generics.ListCreateAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    pagination_class = TaskListPagination
+
 
 class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Task.objects.all()
